ff4d9e4de46dc9c9ce45d0e9754efb6ab0344c7a
sanduni <sanduni2003822madu@gmail.com>
2025-03-02 13:36:01 +0530

    form validation

--- calculation.php
+++ calculation.php
@@ -1,11 +1,1 @@
-<?php
-// Get input from the user
-$unitprice = readline("Enter unit price: ");
-$qty = readline("Enter quantity: ");
 
-// Calculate total price
-$total = $unitprice * $qty;
-
-// Display the result
-echo "Total: " . $total . "\n";
-?>
